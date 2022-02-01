6d6cb8c8e80e958a9eac25a34953cf206a3dee5a
“Debbiepimpo” <debora199318@gmail.com>
2022-02-01 15:37:38 +0100

    Making code working for a specific date and time frame given

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def check_weather_by_ip():
     weather_url= f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}'
     weatherData = requests.get(weather_url).json()
     weather_list = weatherData["list"]
-    mydate = "2022-03-02 00:00:00"
+    mydate = "2022-02-01 15:00:00"
 
     forecast_temp = [item["main"] for item in weather_list if item["dt_txt"] == mydate]
     forecast_weather = [item["weather"] for item in weather_list if item["dt_txt"] == mydate]
-
-    total_forecast = sum(forecast_temp,forecast_weather)
+    
+    total_forecast = forecast_temp + forecast_weather
     return total_forecast
         
 if __name__ == "__main__":

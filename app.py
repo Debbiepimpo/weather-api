9dea9419517c968142ed8cae85fab985ce14844a
Debora Peralta orozco <deboraperaltaorozco@MacBook-Pro-de-Debora.local>
2022-02-02 15:00:24 +0100

    Adding Env variables file  to the project to protect API_KEY

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 import requests
-from datetime import datetime, date
+from datetime import datetime
 
 def check_weather_by_ip():
     ip = "24.48.0.1"
@@ -8,18 +9,18 @@ def check_weather_by_ip():
     lat = ipData["lat"]
     lon = ipData["lon"]
 
-    API_KEY= "b97aac961b6899d1caf2b26a2b88e534"
-    weather_url= f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}'
+    key = os.getenv("API_KEY")
+    key = "b97aac961b6899d1caf2b26a2b88e534"
+
+    weather_url= f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={key}'
     weatherData = requests.get(weather_url).json()
     weather_list = weatherData["list"]
     today = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
     mydate = datetime.strptime(today, "%Y-%m-%d %H:%M:%S")
 
-    forecast_temp = [item["main"] for item in weather_list if datetime.strptime(item["dt_txt"], "%Y-%m-%d %H:%M:%S").date() == mydate.date()]
     forecast_weather = [item["weather"] for item in weather_list if datetime.strptime(item["dt_txt"], "%Y-%m-%d %H:%M:%S").date() == mydate.date()]
     
-    total_forecast = forecast_temp + forecast_weather
-    return total_forecast
+    return forecast_weather
         
 if __name__ == "__main__":
     print(check_weather_by_ip())
